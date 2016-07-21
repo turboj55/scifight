turboj55/scifight

commit cd3ed6b27991ad2e282a02373f0bb653937364a7

scifight/models.py: add forgotten calls to 'super' in validators
Signed-off-by: Pavel Kretov <firegurafiku@gmail.com>

scifight/models.py
@@ -112,6 +112,8 @@ class Fight(models.Model):
     juries      = models.ManyToManyField(Jury, blank=True)
 
     def clean(self):
+        super(models.Model, self).clean()
+
         bad = (self.start_time is not None and
                self.stop_time  is not None and
                self.start_time >= self.stop_time)
@@ -144,6 +146,8 @@ class FightStage(models.Model):
     reviewer    = models.ForeignKey(Participant, related_name="reviewer", null=True, blank=True)
 
     def clean(self):
+        super(models.Model, self).clean()
+
         guys = [self.reporter, self.opponent, self.reviewer]
         guys_uniq = set(guys) - {None}
         if len(guys_uniq) < 2 or len(guys_uniq) + guys.count(None) != 4:
@@ -170,6 +174,8 @@ class JuryPoints(models.Model):
     reviewer_mark = models.IntegerField(null=True, blank=True)
 
     def clean_fields(self, exclude=None):
+        super(models.Model, self).clean_fields(exclude)
+
         exclude_set = set()
         if exclude is not None:
             exclude_set = set(exclude)

commit 33349485344105fb2756343cee378c6b2b84236f

scifight/models.py: fix super() broken in cd3ed6b27991ad
Signed-off-by: Pavel Kretov <firegurafiku@gmail.com>

scifight/models.py
@@ -115,7 +115,7 @@ class Fight(models.Model):
     juries      = models.ManyToManyField(Jury, blank=True)
 
     def clean(self):
-        super(models.Model, self).clean()
+        super().clean()
 
         bad = (self.start_time is not None and
                self.stop_time  is not None and
@@ -152,7 +152,7 @@ class FightStage(models.Model):
     reviewer    = models.ForeignKey(Participant, related_name="reviewer", null=True, blank=True)
 
     def clean(self):
-        super(models.Model, self).clean()
+        super().clean()
 
         guys = [self.reporter, self.opponent, self.reviewer]
         guys_uniq = set(guys) - {None}
@@ -180,7 +180,7 @@ class JuryPoints(models.Model):
     reviewer_mark = models.IntegerField(null=True, blank=True)
 
     def clean_fields(self, exclude=None):
-        super(models.Model, self).clean_fields(exclude)
+        super().clean_fields(exclude)
 
         exclude_set = set()
         if exclude is not None: