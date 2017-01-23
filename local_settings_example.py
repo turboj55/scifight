1a1153e4078365a01dc016ea473e6256be99c925
Pavel Kretov <firegurafiku@gmail.com>
2017-01-23 12:38:55 +0300

    local_setting_example.py: fix missing import
    Make local configuration example consistent with the new approach (more
    specifically, now 'local_settings.py' imports global 'settings.py', not
    vice-versa).
    
    Signed-off-by: Pavel Kretov <firegurafiku@gmail.com>

--- local_settings_example.py
+++ local_settings_example.py
@@ -1,3 +1,14 @@
+# This file is a template for 'local_settings.py' which can be useful when
+# configuring developer's environment. If your setup is as simple as local
+# MariaDB instance, just do:
+#
+#    $ cp local_settings_example.py local_settings.py
+#    $ vim local_settings.py
+#
+# And manually adjust your secret key and database credentials. Consider
+# removing this comment, though.
+from scifight_proj.settings import *
+
 # SECURITY WARNING: keep the secret key used in production secret!
 # You can generate secret key on this site:
 # http://www.miniwebtool.com/django-secret-key-generator/
@@ -14,6 +25,8 @@ DATABASES = {
         'PORT':     3306
     }
 }
+
+LANGUAGE_CODE = 'en-US'
 
 DEBUG = True
 
